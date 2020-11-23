fa94e108f543e4f60f4e9ba67506fb31b992cb58
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2020-11-23 10:32:14 -0600

    rg

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -14,38 +14,100 @@ function onLoad() {
         age++;
     }
     try {
+        console.log("ONLOAD age = " + age);
         document.getElementById("age").innerText = age;
     }
     catch {
         //incase page isn't summary page
+        console.log("ONLOAD age did not work (likely not summary page)");
     }
 
     //set availability for summary page
     var date = new Date();
+    var month = date.getMonth();
     var x = date.getDate() + 14;
     try {
-        if (x >= 29 && date.getMonth() == 1) {//if February
-            x = "March " + x - 29;
+        //set variables
+        {
+            if (x >= 29 && date.getMonth() == 1) {
+                x -= 29;
+                month++;
+            }
+            else if (x >= 30 && (date.getMonth() == 3 || date.getMonth() == 5 || date.getMonth() == 8 || date.getMonth() == 10)) {
+                x -= 30;
+                month++;
+            }
+            else if (x >= 31) {
+                x -= 31;
+                month++;
+            }
+            if (month == 12) {
+                month = 0;
+            }
+            console.log("ONLOAD x = " + x + "; month = " + month);
         }
-        else if (x >= 30) {//if 30 month
-            if (true) {
-                x = 0;
+        //set suffix
+        {
+            x += "";
+            if (x.substring(x.length - 1, x.length) == 1) {
+                x += "st";
+            }
+            else if (x.substring(x.length - 1, x.length) == 2) {
+                x += "nd";
+            }
+            else if (x.substring(x.length - 1, x.length) == 3) {
+                x += "rd";
+            }
+            else {
+                x += "th";
+            }
+            console.log("ONLOAD x = " + x);
+        }
+        //set month
+        {
+            if (month == 0) {
+                x = "Jan " + x;
+            }
+            else if (month == 1) {
+                x = "Feb " + x;
+            }
+            else if (month == 2) {
+                x = "Mar " + x;
+            }
+            else if (month == 3) {
+                x = "Apr " + x;
+            }
+            else if (month == 4) {
+                x = "May " + x;
+            }
+            else if (month == 5) {
+                x = "Jun " + x;
+            }
+            else if (month == 6) {
+                x = "Jul " + x;
+            }
+            else if (month == 7) {
+                x = "Aug " + x;
+            }
+            else if (month == 8) {
+                x = "Sep " + x;
+            }
+            else if (month == 9) {
+                x = "Oct " + x;
+            }
+            else if (month == 10) {
+                x = "Nov " + x;
+            }
+            else {
+                x = "Dec " + x;
             }
         }
-        else if (x >= 31) {//if 31  month
-            if (true) {
-                x = 0;
-            }
-        }
-        else {
-            if (true) {//if same month
-                x = 0;
-            }
-        }
-        document.getElementById("availability").innerText = availability;
+        console.log("ONLOAD x = " + x);
+        document.getElementById("availability").innerText = x;
     }
     catch {
         //incase page isn't summary page
+        console.log("ONLOAD availability did not work (likely not summary page)");
     }
 
     //set styles (other than css) so later code doesnt error from "undefined"
