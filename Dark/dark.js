210dd84aae3262929746db9878369387ab779e86
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2020-12-20 15:13:38 -0600

    e

--- Dark/dark.js
+++ Dark/dark.js
@@ -2,10 +2,10 @@ function $($) { return document.getElementById($);}
 
 function over(x) {
     $(x).style.fontSize = "xx-large";
-    $(x).style.color = "#cccccc";
+    $(x).style.color = "#eeeeee";
 }
 
 function out(x){
     $(x).style.fontSize = "x-large";
-    $(x).style.color = "#888888";
+    $(x).style.color = "#cccccc";
 }
