ba7bcafa65aec1c726d9fc5160e148275adf3be8
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-10-09 16:48:56 -0500

    update JavaScript.js

--- Light/JavaScript.js
+++ Light/JavaScript.js
@@ -3,7 +3,7 @@ var buttonOut = false;
 
 window.onscroll = setButton();
 
-window.onresize = () => {
+document.body.onresize = () => {
 	if (window.innerWidth < 650) {
 		document.getElementById("name").innerHTML = "Sabian";
 	} else {
@@ -16,80 +16,80 @@ window.onload = () => {
 	window.onresize();
 
 	//set age for summary page
-	if (document.getElementById("pageAge").innerText == " "){
+	if (document.getElementById("pageAge").innerText == " ") {
 		var date = new Date();
 		var age = date.getFullYear() - 2001;
 		if (date.getMonth() > 4 && date.getDate() > 1) {
 			age++;
 		}
-			document.getElementById("pageAge").innerText = age++ + " year old ";
+		document.getElementById("pageAge").innerText = age++ + " year old ";
 	}
 
 	//set availability for summary page
-	if(document.getElementById.innerHTML == "2 weeks from now"){
+	if (document.getElementById.innerHTML == "2 weeks from now") {
 		var month = date.getMonth();
 		var x = date.getDate() + 14;
-			//set variables
-			{
-				if (x >= 29 && date.getMonth() == 1) {
-					x -= 29;
-					month++;
-				} else if (x >= 30 && (date.getMonth() == 3 || date.getMonth() == 5 || date.getMonth() == 8 || date.getMonth() == 10)) {
-					x -= 30;
-					month++;
-				} else if (x >= 31) {
-					x -= 31;
-					month++;
-				}
-				if (month == 12) {
-					month = 0;
-				}
-				console.log("ONLOAD x = " + x + "; month = " + month);
+		//set variables
+		{
+			if (x >= 29 && date.getMonth() == 1) {
+				x -= 29;
+				month++;
+			} else if (x >= 30 && (date.getMonth() == 3 || date.getMonth() == 5 || date.getMonth() == 8 || date.getMonth() == 10)) {
+				x -= 30;
+				month++;
+			} else if (x >= 31) {
+				x -= 31;
+				month++;
 			}
-			//set suffix
-			{
-				x += "";
-				if (x.substring(x.length - 1, x.length) == 1) {
-					x += "st";
-				} else if (x.substring(x.length - 1, x.length) == 2) {
-					x += "nd";
-				} else if (x.substring(x.length - 1, x.length) == 3) {
-					x += "rd";
-				} else {
-					x += "th";
-				}
-				console.log("ONLOAD x = " + x);
+			if (month == 12) {
+				month = 0;
 			}
-			//set month
-			{
-				if (month == 0) {
-					x = "Jan " + x;
-				} else if (month == 1) {
-					x = "Feb " + x;
-				} else if (month == 2) {
-					x = "Mar " + x;
-				} else if (month == 3) {
-					x = "Apr " + x;
-				} else if (month == 4) {
-					x = "May " + x;
-				} else if (month == 5) {
-					x = "Jun " + x;
-				} else if (month == 6) {
-					x = "Jul " + x;
-				} else if (month == 7) {
-					x = "Aug " + x;
-				} else if (month == 8) {
-					x = "Sep " + x;
-				} else if (month == 9) {
-					x = "Oct " + x;
-				} else if (month == 10) {
-					x = "Nov " + x;
-				} else {
-					x = "Dec " + x;
-				}
+			console.log("ONLOAD x = " + x + "; month = " + month);
+		}
+		//set suffix
+		{
+			x += "";
+			if (x.substring(x.length - 1, x.length) == 1) {
+				x += "st";
+			} else if (x.substring(x.length - 1, x.length) == 2) {
+				x += "nd";
+			} else if (x.substring(x.length - 1, x.length) == 3) {
+				x += "rd";
+			} else {
+				x += "th";
 			}
 			console.log("ONLOAD x = " + x);
-			availability.innerText = x;
+		}
+		//set month
+		{
+			if (month == 0) {
+				x = "Jan " + x;
+			} else if (month == 1) {
+				x = "Feb " + x;
+			} else if (month == 2) {
+				x = "Mar " + x;
+			} else if (month == 3) {
+				x = "Apr " + x;
+			} else if (month == 4) {
+				x = "May " + x;
+			} else if (month == 5) {
+				x = "Jun " + x;
+			} else if (month == 6) {
+				x = "Jul " + x;
+			} else if (month == 7) {
+				x = "Aug " + x;
+			} else if (month == 8) {
+				x = "Sep " + x;
+			} else if (month == 9) {
+				x = "Oct " + x;
+			} else if (month == 10) {
+				x = "Nov " + x;
+			} else {
+				x = "Dec " + x;
+			}
+		}
+		console.log("ONLOAD x = " + x);
+		availability.innerText = x;
 	}
 }
 
