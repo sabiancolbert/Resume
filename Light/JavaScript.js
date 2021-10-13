bff8e5afd3e1520d8653754bd6c756a0f1625ebb
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-10-12 19:56:09 -0500

    update JavaScript.js

--- Light/JavaScript.js
+++ Light/JavaScript.js
@@ -100,12 +100,12 @@ function setTitle() {
 				try {
 					document.getElementById("edu").innerHTML = "Edu.";
 				}catch {
-					try{
-					document.getElementById("docs").innerHTML = "Docs";
-}
-catch {
-	document.getElementById("port").innerHTML = "Port.";
-}
+					try {
+						document.getElementById("docs").innerHTML = "Docs";
+					}
+					catch {
+						document.getElementById("port").innerHTML = "W.I.P.";
+					}
 				}
 			}
 		}
@@ -121,102 +121,102 @@ catch {
 				try {
 					document.getElementById("edu").innerHTML = "Education";
 				}catch {
-					try{
-					document.getElementById("docs").innerHTML = "Documents";
-					}catch{
-						document.getElementById("port").innerHTML = "Portfolio";
+					try {
+						document.getElementById("docs").innerHTML = "Documents";
+					}catch {
+						document.getElementById("port").innerHTML = "Work In Progress";
+					}
 				}
 			}
 		}
 	}
-}
 
-/* themes button */
-function theme() {
-	window.location.replace("/themes.html");
-}
+	/* themes button */
+	function theme() {
+		window.location.replace("/themes.html");
+	}
 
-/* determine menu button position */
-function isMouse() {
-	mouse = true;
-}
+	/* determine menu button position */
+	function isMouse() {
+		mouse = true;
+	}
 
-/* set menu button position */
-function setButton() {
-	toggleMenu(false, false);
+	/* set menu button position */
+	function setButton() {
+		toggleMenu(false, false);
 
-	//scroll is >=40px, allow menu button to hide (if scroll is < 40 it won't appear to change yet)
-	if (mouse && document.documentElement.scrollTop >= 40) {
-		if (!buttonRevealed) {
-			menuButton.style.top = "-40px";
+		//scroll is >=40px, allow menu button to hide (if scroll is < 40 it won't appear to change yet)
+		if (mouse && document.documentElement.scrollTop >= 40) {
+			if (!buttonRevealed) {
+				menuButton.style.top = "-40px";
+			}
+			menuButton.style.position = "fixed";
+
 		}
-		menuButton.style.position = "fixed";
 
-	}
+		//scroll is NOT >=40px, allow menu button to reveal
+		else if (mouse) {
+			menuButton.style.top = "0px";
+			menuButton.style.position = "absolute";
+		}
 
-	//scroll is NOT >=40px, allow menu button to reveal
-	else if (mouse) {
-		menuButton.style.top = "0px";
-		menuButton.style.position = "absolute";
-	}
-
-	//if mobile, fix button
-	else {
-		menuButton.style.top = "0px";
-		menuButton.style.position = "fixed";
-	}
-}
-
-/* reveal menu button */
-function revealButton(override = false) {
-	buttonRevealed = true;
-
-	//if button needs revealed, then reveal button
-	if (menuButton.style.top == "-40px" || override) {
-		var animation = setInterval(() => {
-			if (menuButton.style.top == "0px" || !buttonRevealed) {
-				clearInterval(animation);
-				if (!buttonRevealed) {
-					hideButton(true);
-				}
-			} else {
-				menuButton.style.top = Number(menuButton.style.top.substring(0, menuButton.style.top.length - 2)) + 2 + "px";
-			}
-		},
-			10);
-	}
-}
-
-/* hide menu button */
-function hideButton(override = false) {
-	buttonRevealed = false;
-
-	//if button needs hidden, then hide button
-	if ((menuButton.style.top == "0px" || override) && document.documentElement.scrollTop >= 54) {
-		var animation = setInterval(() => {
-			if (menuButton.style.top == "-40px" || buttonRevealed) {
-				clearInterval(animation);
-				if (buttonRevealed) {
-					revealButton(true);
-				}
-			} else {
-				menuButton.style.top = Number(menuButton.style.top.substring(0, menuButton.style.top.length - 2)) - 2 + "px";
-			}
-		},
-			10);
-	}
-}
-
-/* toggle nav menu */
-function toggleMenu(open, setbutton = true) {
-	if (open) {
-		nav.hidden = false;
-		menuButton.hidden = true;
-	} else {
-		nav.hidden = true;
-		menuButton.hidden = false;
-		if (setbutton) {
-			setButton();
+		//if mobile, fix button
+		else {
+			menuButton.style.top = "0px";
+			menuButton.style.position = "fixed";
 		}
 	}
-}
+
+	/* reveal menu button */
+	function revealButton(override = false) {
+		buttonRevealed = true;
+
+		//if button needs revealed, then reveal button
+		if (menuButton.style.top == "-40px" || override) {
+			var animation = setInterval(() => {
+				if (menuButton.style.top == "0px" || !buttonRevealed) {
+					clearInterval(animation);
+					if (!buttonRevealed) {
+						hideButton(true);
+					}
+				} else {
+					menuButton.style.top = Number(menuButton.style.top.substring(0, menuButton.style.top.length - 2)) + 2 + "px";
+				}
+			},
+				10);
+		}
+	}
+
+	/* hide menu button */
+	function hideButton(override = false) {
+		buttonRevealed = false;
+
+		//if button needs hidden, then hide button
+		if ((menuButton.style.top == "0px" || override) && document.documentElement.scrollTop >= 54) {
+			var animation = setInterval(() => {
+				if (menuButton.style.top == "-40px" || buttonRevealed) {
+					clearInterval(animation);
+					if (buttonRevealed) {
+						revealButton(true);
+					}
+				} else {
+					menuButton.style.top = Number(menuButton.style.top.substring(0, menuButton.style.top.length - 2)) - 2 + "px";
+				}
+			},
+				10);
+		}
+	}
+
+	/* toggle nav menu */
+	function toggleMenu(open, setbutton = true) {
+		if (open) {
+			nav.hidden = false;
+			menuButton.hidden = true;
+		} else {
+			nav.hidden = true;
+			menuButton.hidden = false;
+			if (setbutton) {
+				setButton();
+			}
+		}
+	}
