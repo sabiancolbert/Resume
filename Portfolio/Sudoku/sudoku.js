75557391f52a655c556caf2cd173f7e6560ac58e
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-11-28 14:38:11 -0600

    update sudoku.js

--- Portfolio/Sudoku/sudoku.js
+++ Portfolio/Sudoku/sudoku.js
@@ -55,7 +55,7 @@ function setCells() {
 
 function testVertical(currentCell, number) {
   var availability = true;
-  currentCell = 9;
+  
   if (currentCell > 8) {
     for (i = currentCell-9; i > -1; i -= 9) {
       /*if (cells[i] == number) {
