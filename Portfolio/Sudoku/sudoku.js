feae807efd5d6b29439e8f08fd77a311f4ee08fe
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-11-28 14:20:20 -0600

    update sudoku.js

--- Portfolio/Sudoku/sudoku.js
+++ Portfolio/Sudoku/sudoku.js
@@ -66,6 +66,7 @@ function testVertical(currentCell, number) {
 function testHorizonal(currentCell, number) {
   var availability = true;
   var rowStart = Math.floor(currentCell/9)*9+1;
+  c("START "+rowStart);
   for (i = rowStart; i < rowStart+9; i++) {
     if (cells[i] == number) {
       availability = false;
