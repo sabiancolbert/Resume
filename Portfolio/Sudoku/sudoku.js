9af4231139ffccb139a39a71bd8b92f478ed0d3d
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-11-28 12:03:55 -0600

    update sudoku.js

--- Portfolio/Sudoku/sudoku.js
+++ Portfolio/Sudoku/sudoku.js
@@ -18,7 +18,7 @@ function setCells() {
       number = Math.floor(Math.random()*9+1);
       if (!attemptedNumbers[currentCell].includes(number)) {
         attemptedNumbers[currentCell].push(number);
-        if (vertical(currentCell, number) && horizontal(currentCell, number) && box(currentCell, number)) {
+        if (testVertical(currentCell, number) && testHorizonal(currentCell, number) && testBox(currentCell, number)) {
           //if(rules){
           //HERE
           invalid = false;
@@ -47,19 +47,50 @@ function setCells() {
 
 function verticle(currentCell, number) {
   var availability = true;
-  for(i=currentCell-9;i>0;i-9){
-    if(cells[i] == number){
+  for (i = currentCell-9; i > 0; i-9) {
+    if (cells[i] == number) {
       availability = false;
     }
   }
   return availability;
 }
 
-function horizontal(currentCell, number) {
-  
+function testHorizonal(currentCell, number) {
+  var availability = true;
+  var rowStart = Math.floor(currentCell/9)*9+1;
+  for(i=rowStart;i< rowStart+9;i++){
+    if(cells[i]){
+      availability=false;
+    }
+  }
+  return availability;
 }
 
-function box(currentCell, number) {}
+function testBox(currentCell, number) {
+  var availability = true;
+  var stop = currentCell -9;
+  var adjust = -2;
+  var temp = Math.floor(currentCell/9-.1+1)/3-.1+1+" ";
+  if(temp.includes(".5")){
+    stop -= 9;
+  }
+  else if(temp.includes(".9")){
+    stop-=18;
+  }
+  temp = currentCell / 3 +" ";
+  if(temp.includes(".6")){
+    adjust= -1;
+  }
+  else if (temp.includes(".3")){
+    adjust = 0;
+  }
+  for(i=currentCell;i>stop;i-9){
+    if(cells[i+adjust]==number||cells[i+adjust+1]==number||cells[i+adjust+2]==number){
+      availability=false;
+    }
+  }
+  return availability;
+}
 
 function fail() {}
 
