d38cafec38d395af1b1e750aad9541985acdf146
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-12-03 14:58:28 -0600

    update sudoku.js

--- Portfolio/Sudoku/sudoku.js
+++ Portfolio/Sudoku/sudoku.js
@@ -258,7 +258,7 @@ function set(cell) {
     document.getElementById(cell).innerHTML = document.getElementById("selection").innerHTML;
     currentMove++;
     numberTotals[document.getElementById(cell).innerHTML - 1]++;
-
+document.getElementById("numberLabel").innerHTML = 1 +//HERE HERE
     //add to display
     //moveList[currentMove] =
     //HERE
