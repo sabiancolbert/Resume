2c1d5f300ecdf8b53624c729016045fe8a04649a
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-12-11 09:18:53 -0600

    update sudoku.js

--- Portfolio/Sudoku/sudoku.js
+++ Portfolio/Sudoku/sudoku.js
@@ -276,7 +276,6 @@ function select(number) {
   c("select("+number+")")
   //dont clear highlight, for visually easier erasing
   if (number != 0) {
-    c("&"+counterElement);
     counterElement.innerHTML = numberTotals[number-1];
     selectionElement.innerHTML = number;
     //HERE change highlight
