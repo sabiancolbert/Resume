09e5f367c1d5d7da3ca1140b8f88505f7273b880
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-12-12 15:49:53 -0600

    update sudoku.js

--- Portfolio/Sudoku/sudoku.js
+++ Portfolio/Sudoku/sudoku.js
@@ -342,13 +342,13 @@ function set(id, useSelection = true) {
     }
     /* Note Number */
     else if (noteMode == 2) {
-      /* Remove Number From Cell */
+      /* Remove Note From Cell */
       if (noteCells[cellNumber][selectionElement.innerHTML] > 0) {
         c("set - remove note from cell");
         document.getElementById("n"+selectionElement.innerHTML+cellNumber).style.visibility = "hidden";
         noteCells[cellNumber][selectionElement.innerHTML] = 0;
       }
-      /* Add Number To Cell */
+      /* Add Note To Cell */
       else {
         c("set - add note to cell");
         if (displayCells[cellNumber] !=-1) {
