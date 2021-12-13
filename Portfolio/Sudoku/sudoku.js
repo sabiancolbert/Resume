7a13a2380553cf19f34f670410f92516cea8e95d
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-12-12 18:28:46 -0600

    update sudoku.js

--- Portfolio/Sudoku/sudoku.js
+++ Portfolio/Sudoku/sudoku.js
@@ -285,17 +285,18 @@ function isDefaultCell(cell) {
 function select(number) {
   c("select("+number+")")
   //set grid highlight
-    for (i = 0; i < 81; i++) {
-      var cellElement = document.getElementById("c"+i);
-  if (number != displayCells[i]) {
-    cellElement.style.backgroundColor = "#ccccee";
-    if (cellElement.style.fontSize == "125%") {
-      cellElement.style.color = "#777777";
-    }
-  } else {
-    cellElement.style.backgroundColor = "#3388dd";
-    if (cellElement.style.fontSize == "125%") {
-      cellElement.style.color = "#ccccee";
+  for (i = 0; i < 81; i++) {
+    var cellElement = document.getElementById("c"+i);
+    if (number != displayCells[i]) {
+      cellElement.style.backgroundColor = "#ccccee";
+      if (cellElement.style.fontSize == "125%") {
+        cellElement.style.color = "#777777";
+      }
+    } else {
+      cellElement.style.backgroundColor = "#3388dd";
+      if (cellElement.style.fontSize == "125%") {
+        cellElement.style.color = "#ccccee";
+      }
     }
   }
   if (number != 0) {
