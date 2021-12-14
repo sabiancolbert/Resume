43786bc7a640f008a1aa96c131802d94bea481b9
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-12-13 22:22:49 -0600

    update sudoku.js

--- Portfolio/Sudoku/sudoku.js
+++ Portfolio/Sudoku/sudoku.js
@@ -355,8 +355,10 @@ function set(cellId, changingMove = false) {
     if (cellElement.innerHTML == " ") {
       cellNoteMode==-1;
     }
-    moveList.push([cellId, cellElement.innerHTML, cellNoteMode])
-    if (changingMove) {
+c(moveList);
+    moveList.push([cellId, cellElement.innerHTML, cellNoteMode]);
+c(moveList);
+if (changingMove) {
       newHTML = moveList[currentMove][1];
       cellNoteMode = moveList[currentMove][2];
     } else {
