cab6acdd835e2010d86a32d08746f75a80677cb7
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-12-14 08:24:43 -0600

    update sudoku.js

--- Portfolio/Sudoku/sudoku.js
+++ Portfolio/Sudoku/sudoku.js
@@ -384,7 +384,7 @@ function set(cellId, direction = 0) {
       cellElement.innerHTML = " ";
       displayCells[cellNumber] = 0;
       noteCells[cellNumber] = [];
-      cellElement.style.backgroundColor = "#ccccee";
+      cellElement.style.backgroundColor = "red";
     }
     /* Note Number */
     else if (cellNoteMode == 2) {
