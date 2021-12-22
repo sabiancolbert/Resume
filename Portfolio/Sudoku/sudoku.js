c1d2f2b5fa8850ef0f02bea0f930d51ebcf26d83
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-12-22 12:49:39 -0600

    update sudoku.js

--- Portfolio/Sudoku/sudoku.js
+++ Portfolio/Sudoku/sudoku.js
@@ -145,8 +145,7 @@ function setGrid() {
   c("setGrid()");
   /* Unsolve */
   var tested = new Array([0]);
-  c((Math.floor(Math.random()*5))*1+difficulty);
-  var stop = 81 - Math.floor(Math.random()*5+difficulty);
+  var stop = 81 - Math.floor(Math.random()*5+parseInt(difficulty));
   while (stop > 0 && tested.length < 81) {
     var cellNumber = Math.floor(Math.random()*81);
     if (!tested.includes(cellNumber)) {
