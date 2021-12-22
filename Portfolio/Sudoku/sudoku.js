435ec7664732865270eedf9d34ace91199e65cd8
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-12-22 12:03:51 -0600

    update sudoku.js

--- Portfolio/Sudoku/sudoku.js
+++ Portfolio/Sudoku/sudoku.js
@@ -345,7 +345,6 @@ function set(cellId, direction = 0) {
     //HERE HERE sooooometimes erasing and undoing will send a note through as a number (cellnotemode wrong)
     var cellElement = document.getElementById(cellId);
     var cellNoteMode = 0;
-    c(cellNoteMode);
     if (cellElement.innerHTML == " ") {
       cellNoteMode==-1;
     } else if (displayCells[cellNumber]==-1) {
@@ -380,7 +379,6 @@ function set(cellId, direction = 0) {
         counterElement.innerHTML = numberTotals[cellElement.innerHTML];
       }
     }
-    c(cellNoteMode);
     /* Erase */
     if (cellNoteMode == -1) {
       c("set - erase cell");
