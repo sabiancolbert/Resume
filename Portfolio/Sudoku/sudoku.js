80fc6a93ed378d466db8beb67474271d27099507
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-12-22 20:35:35 -0600

    update sudoku.js

--- Portfolio/Sudoku/sudoku.js
+++ Portfolio/Sudoku/sudoku.js
@@ -432,9 +432,7 @@ function set(cellId, direction = 0) {
         cellElement.style.color = "black";
         cellElement.style.backgroundColor = "#ccccee";
         cellElement.style.fontSize = "75%";
-        //HERE HERE HERE selection not defined
-        c(document.getElementById("n"+document.getElementById("selectionElement").innerHTML+cellNumber));
-        document.getElementById("n"+document.getElementById("selectionElement").innerHTML+cellNumber).backgroundColor = "#3388dd";
+        document.getElementById("n"+document.getElementById("selectionElement").innerHTML+cellNumber).style.backgroundColor = "#3388dd";
         if (newHTML.includes("div")) {
           cellElement.innerHTML = newHTML;
         }
