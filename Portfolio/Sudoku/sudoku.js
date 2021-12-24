3fb06de1f736fee15dd5dc2d3ea4465523165cbf
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-12-23 19:23:15 -0600

    update sudoku.js

--- Portfolio/Sudoku/sudoku.js
+++ Portfolio/Sudoku/sudoku.js
@@ -432,12 +432,10 @@ function set(cellId, direction = 0) {
         cellElement.style.color = "black";
         cellElement.style.backgroundColor = "#ccccee";
         cellElement.style.fontSize = "75%";
-        c("n"+document.getElementById("selectionElement").innerHTML+cellNumber);
-        document.getElementById("n"+document.getElementById("selectionElement").innerHTML+cellNumber).style.visibility = "visible";
-        document.getElementById("n"+document.getElementById("selectionElement").innerHTML+cellNumber).style.backgroundColor = "#3388dd";
         if (newHTML.includes("div")) {
           cellElement.innerHTML = newHTML;
         }
+        document.getElementById("n"+document.getElementById("selectionElement").innerHTML+cellNumber).style.backgroundColor = "#3388dd";
       }
     }
     /* Regular Number */
