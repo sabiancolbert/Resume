837b88b46a19bb0a7e06aeb3f47a0d23003d4db4
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-12-25 14:34:59 -0600

    update sudoku.js

--- Portfolio/Sudoku/sudoku.js
+++ Portfolio/Sudoku/sudoku.js
@@ -397,7 +397,7 @@ function set(cellId, direction = 0) {
          cellNoteMode = 0;
       }
       //counter element
-      if (displayCells[cellNumber] > 0) {
+      if (displayCells[cellNumber] > -2) {
         //HERE HERE HERE
          numberTotals[cellElement.innerHTML]++;
          if (cellElement.innerHTML == selectionElement.innerHTML) {
