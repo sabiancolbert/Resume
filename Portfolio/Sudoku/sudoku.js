eecf12aff1bb9f9153be71ec946439b3fc58d2f1
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-12-29 09:07:10 -0600

    update sudoku.js

--- Portfolio/Sudoku/sudoku.js
+++ Portfolio/Sudoku/sudoku.js
@@ -39,7 +39,7 @@ function sizePage() {
     c("b");
     }
       short = width;
-    buttonBox.style="top:49.9%;margin-left:0";
+    buttonBox.style.margin:"49.9% 0 0 0";
   } 
     /* Landscape */
   else {
@@ -52,7 +52,7 @@ function sizePage() {
       c("d");
     }
     short = height;
-    buttonBox.style="top:0;margin-left:49.9%";
+    buttonBox.style.margin:"0 0 0 49.9%";
   }
   /* Set Css */
   c("short"+short+"height"+height+"width"+width);
