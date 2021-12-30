e52b7df5ce5f753907353d7577f7fabef17039c9
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-12-30 11:29:18 -0600

    update sudoku.js

--- Portfolio/Sudoku/sudoku.js
+++ Portfolio/Sudoku/sudoku.js
@@ -439,6 +439,7 @@ function set(cellNumber, direction = 0) {
   if (userCells.includes(cellNumber)) {
     var cellElement = getCell(cellNumber);
     var cellNoteMode = displayCells[cellNumber];
+    c("HERE HERE "+cellNoteMode);
     var content = selectionElement.innerHTML;
     if (content == " " || content == cellElement.innerHTML && selectionNoteMode == cellNoteMode && cellNoteMode > -2) {
       cellNoteMode = 0;
