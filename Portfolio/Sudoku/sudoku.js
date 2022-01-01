8e03d8e1948d4f946db5573af5c9d907bf229990
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2022-01-01 10:37:24 -0600

    update sudoku.js

--- Portfolio/Sudoku/sudoku.js
+++ Portfolio/Sudoku/sudoku.js
@@ -21,7 +21,7 @@ function c(c) {
 }
 
 function getCell(cellNumber) {
-  c(cellNumber+"&"+(cellNumber + Math.floor(cellNumber/9)));
+ c(document.getElementById("gridBox").children[cellNumber + Math.floor(cellNumber/9)]);
   return document.getElementById("gridBox").children[cellNumber + Math.floor(cellNumber/9)];
 }
 
