ffa1dbff944b2ed0a595239616ffcb1986954d2e
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2022-01-08 21:10:41 -0600

    update sudoku.js

--- Portfolio/Sudoku/sudoku.js
+++ Portfolio/Sudoku/sudoku.js
@@ -431,7 +431,7 @@ function select(selection) {
 function set(cellNumber, direction = 0) {
   c("set" +cellNumber);
   c(currentSelection +"&"+getCell(cellNumber).innerHTML+"&");
-  if (userCells.includes(cellNumber)){//HERE HERE HERE && (getCell(cellNumber).innerHTML!="" || currentSelection != 0)) {
+  if (userCells.includes(cellNumber) && (getCell(cellNumber).innerHTML!="&nbsp" || currentSelection != 0)) {
     var cellElement = getCell(cellNumber);
     var cellNoteMode = displayCells[cellNumber];
     var content = selectionElement.innerHTML;
