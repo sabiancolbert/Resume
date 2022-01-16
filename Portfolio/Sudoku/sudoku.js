4510d779efdaf4eabb953424e845fb0ac46ecfc0
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2022-01-16 09:29:49 -0600

    update sudoku.js

--- Portfolio/Sudoku/sudoku.js
+++ Portfolio/Sudoku/sudoku.js
@@ -148,9 +148,7 @@ function decideGridNumbers() {
         "isLocked": false,
         "isWrong": false
       };
-      c(cells[currentCell]);
-      c("&&");
-      c("cell display: "+cells[currentCell].display);
+      c("&&&"+cells[currentCell]);
     }
     /* Set Every Cell's Info (cells[0-80]) */
     while (currentCell > 0 && currentCell < 82) {
