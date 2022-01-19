fb64c3a9305d103e53fa5a905249e2d83c404f62
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2022-01-19 07:37:36 -0600

    update sudoku.js

--- Portfolio/Sudoku/sudoku.js
+++ Portfolio/Sudoku/sudoku.js
@@ -148,7 +148,6 @@ function decideGridNumbers() {
       };
     }
     /* Set Every Cell's Info (cells[0-80]) */
-    c(currentCell +"&&");
     while (currentCell > 0 && currentCell < 82) {
       var number = 0;
       var invalid = true;
