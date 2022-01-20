27fac37ebbace86d15ad40df4814bbecbb951191
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2022-01-20 08:30:41 -0600

    update sudoku.js

--- Portfolio/Sudoku/sudoku.js
+++ Portfolio/Sudoku/sudoku.js
@@ -229,7 +229,7 @@ function isInVertical(cell, number) {
 //search for the same number in the same row
 function isInHorizonal(cell, number) {
   var result = false;
-  var rowStart = Math.floor(cell/9) * 9;
+  var rowStart = Math.floor(cell/9) * 9 +1;
   for (i = rowStart; i < rowStart+9; i++) {
     c(i);
     if (cells[i].display == number) {
