5bcf0481947a38b5aac7d67af3f6cbfa0637e334
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2022-01-21 11:08:16 -0600

    update sudoku.js

--- Portfolio/Sudoku/sudoku.js
+++ Portfolio/Sudoku/sudoku.js
@@ -344,6 +344,7 @@ function isDefaultCell(cell) {
   //HERE test noteCells, use dummy numbers for next part
   /* Find Empty Cells In Row */
   var temp = Math.floor(cell / 9) * 9;
+  c(temp);
   for (i = temp; i < temp + 9; i++) {
     if (cells[i].display == 0) {
       emptyCells.push(i);
