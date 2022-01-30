8a90c41015f35de6cf64a4af4fd7d4cc184674ea
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2022-01-29 18:55:05 -0600

    update sudoku.js

--- Portfolio/Sudoku/sudoku.js
+++ Portfolio/Sudoku/sudoku.js
@@ -2,7 +2,7 @@
 //one extra cell for readability (82 cells instead of 81 cells so that cells[1] is the first cell instead of cells[0]~)
 var cells = new Array(1);
 //one extra for readability (10 instead of 9)
-var numberTotals = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+var numberTotals = new Array(0,9,9,9,9,9,9,9,9,9);
 //for game creation
 var currentMove = 0;
 var difficulty = 0;
