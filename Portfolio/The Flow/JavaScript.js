e1dfeeafac8232b75cd336c11d390671f1f83b60
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2022-07-07 11:17:38 -0500

    update JavaScript.js

--- Portfolio/The Flow/JavaScript.js
+++ Portfolio/The Flow/JavaScript.js
@@ -18,37 +18,18 @@ function resize() {
     var left = 0;
     /* Portrait */
     if (width < height) {
-      if (width * 2 < height) {
-        top = height / 2 + "px";
-        right = 0;
-        bottom = height / 2 + "px";
-        left = 0;
-      } else {
-        top = height / 2 + "px";
-        right = width / 2 + "px";
-        bottom = height / 2 + "px";
-        left = width / 2 + "px";
-      }
+      words.style.bottom="50%";
+      words.style.right=0;
+      buttons.style.top="50%";
+      buttons.style.left=0;
     }
     /* Landscape */
     else {
-      if (height * 2 < width) {
-        top = 0;
-        right = width / 2 + "px";
-        bottom = 0;
-        left = width / 2 + "px";
-      } else {
-        top = height / 2 + "px";
-        right = width / 2 + "px";
-        bottom = height / 2 + "px";
-        left = width / 2 + "px";
-      }
+      words.style.bottom=0;
+      words.style.right="50%";
+      buttons.style.top=0;
+      buttons.style.left="50%";
     }
-    /* Set Css */
-    buttons.style.top = "50%";
-    buttons.style.left = "0";
-    words.style.bottom = "50%";
-    words.style.right = "0";
     sizingPage = false;
   }
 }
