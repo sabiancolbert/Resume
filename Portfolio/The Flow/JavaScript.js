ac2c97d3f6c27495b30f91cf509eee387c2dae10
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2022-07-08 12:17:10 -0500

    update JavaScript.js

--- Portfolio/The Flow/JavaScript.js
+++ Portfolio/The Flow/JavaScript.js
@@ -21,11 +21,11 @@ function resize() {
     var left = 0;
     /* Portrait */
     if (width < height) {
-      words.style.bottom = "100vw";
+      words.style.bottom = "50%";
       words.style.right = 0;
       buttons.style.top = "50%";
       buttons.style.left = 0;
-      bodyimg.style.right = "25vw";
+      bodyimg.style.right = "70%";
       info.style.maxHeight = "70%";
       body.style.minHeight = "200vmin";
       body.style.minWidth = null;
