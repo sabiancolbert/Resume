262ce5ff050ce9f50a262b620cfcf7f8987993bb
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2022-07-08 11:50:53 -0500

    update JavaScript.js

--- Portfolio/The Flow/JavaScript.js
+++ Portfolio/The Flow/JavaScript.js
@@ -26,7 +26,7 @@ function resize() {
       buttons.style.top = "50%";
       buttons.style.left = 0;
       bodyimg.style.right = "25vw";
-      info.style.maxHeight = "80%";
+      info.style.maxHeight = "85%";
       body.style.minHeight = "200vmin";
       body.style.minWidth = null;
     }
