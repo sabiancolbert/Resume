746f77ec03d2e58c8434de2a184264be8b20e1b8
Sabian Colbert <31480512+sabiancolbert@users.noreply.github.com>
2023-04-12 07:28:57 -0500

    Update JavaScript.js

--- Tokens/JavaScript.js
+++ Tokens/JavaScript.js
@@ -8,9 +8,12 @@ var kaiTokens =
 var jax = document.getElementById("jax");
 var kai = document.getElementById("kai");
 
-function updateTokens(){
+function updateTokens(){/*
 jax.innerHTML = jaxTokens + " Tokens";
 kai.innerHTML = kaiTokens + " Tokens";
+*/
+jax.innerHTML ="Nope";
+kai.innerHTML = getTokens();
 }
 
 function jaxUp(){
@@ -31,4 +34,15 @@ updateTokens();
 function kaiDown(){
 kaiTokens-=1;
 updateTokens();
+}
+
+function getTokens() {
+  var result = null;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", "tokens.txt", false);
+  xmlhttp.send();
+  if (xmlhttp.status==200) {
+    result = xmlhttp.responseText;
+  }
+  return result;
 }
