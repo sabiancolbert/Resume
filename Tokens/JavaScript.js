340e06959c0e1ef02682b55e27729db712c2b1a1
Sabian Colbert <31480512+sabiancolbert@users.noreply.github.com>
2023-04-12 08:14:04 -0500

    Update JavaScript.js

--- Tokens/JavaScript.js
+++ Tokens/JavaScript.js
@@ -1,4 +1,4 @@
-//const fs = require(“fs”);
+const fs = require(“fs”);
 
 var jaxTokens =
 2
@@ -12,7 +12,7 @@ var kai = document.getElementById("kai");
 
 
 function updateTokens(){
-jax.innerHTML = jaxTokens + " Tokens";
+jax.innerHTML = jaxTokens + " Tokensgh";
 kai.innerHTML = kaiTokens + " Tokens";
 /*
 jax.innerHTML ="nada";
