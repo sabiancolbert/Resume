c2c85d7a77d99aebb4c86c59f4d71c4c6b8c9083
Sabian Colbert <31480512+sabiancolbert@users.noreply.github.com>
2023-04-12 09:43:09 -0500

    Update test.js

--- Test/test.js
+++ Test/test.js
@@ -1,8 +1,6 @@
 var http = require('http')
 var fs = require('fs')
 
-const PORT = 8080
-
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('<html><body><p>worked</p></body></html>');
