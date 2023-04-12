a8512ee0917c3d9d081aa2b01d46bfba059848ef
Sabian Colbert <31480512+sabiancolbert@users.noreply.github.com>
2023-04-12 12:39:25 -0500

    Update express.js

--- Tokens/src/loaders/express.js
+++ Tokens/src/loaders/express.js
@@ -51,7 +51,7 @@ export default (app) => {
   });
 
   app.use((req, res, next) => {
-   // res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Content-Security-Policy-Report-Only', 'default-src: https:');
