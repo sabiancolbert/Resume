30da03336be098c7830792b00211a812c87c62a3
Sabian Colbert <31480512+sabiancolbert@users.noreply.github.com>
2023-04-16 09:16:12 -0500

    Create JavaScript.js

---
+++ Tokens/JavaScript.js
@@ -0,0 +1,17 @@
+
+function onLoad(){
+var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", "tokens.txt", false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
