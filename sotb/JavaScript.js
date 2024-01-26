dd3d6e14ca0138a77d1f91c535ca703b4d90a455
Sabian Colbert <31480512+sabiancolbert@users.noreply.github.com>
2024-01-26 16:26:11 -0600

    Update JavaScript.js

--- sotb/JavaScript.js
+++ sotb/JavaScript.js
@@ -1,6 +1,20 @@
 var podMode = 0;
 var podRunning = false;
 
+(function () {
+    var old = console.log;
+    var logger = document.getElementById('log');
+    console.log = function () {
+      for (var i = 0; i < arguments.length; i++) {
+        if (typeof arguments[i] == 'object') {
+            logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(arguments[i], undefined, 2) : arguments[i]) + '<br />';
+        } else {
+            logger.innerHTML += arguments[i] + '<br />';
+        }
+      }
+    }
+})();
+
 function expandPod(){
 podMode = -1;
   if(!podRunning){
