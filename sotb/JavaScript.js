ee2ef4d711a13432c930e00ed37303c66f01ceab
Sabian Colbert <31480512+sabiancolbert@users.noreply.github.com>
2024-01-26 15:59:51 -0600

    Update JavaScript.js

--- sotb/JavaScript.js
+++ sotb/JavaScript.js
@@ -1,16 +1,32 @@
-function expandPodBox() {
+var podMode = 0;
+var podRunning = false;
+
+function expandPod(){
+podMode = -1;
+  if(!podRunning){
+    animatePod();
+  }
+}
+
+function animatePod() {
+  podRunning = true;
   let id = null;
-  const elem = document.getElementById("podButton");   
-  let pos = 0;
-  clearInterval(id);
-  id = setInterval(frame, 5);
+  const pod = document.getElementById("podButton");
+  let podPos = pod.style.top.split('v')[0];
+  let podSize = pod.style.width.split('v')[0];
+  clearMode(id);
+  id = setMode(frame, 10);
   function frame() {
-    if (pos == 350) {
-      clearInterval(id);
+    if (podPos <= 0.1 || podPos >= 4.5) {
+      podRunning = false;
+      clearMode(id);
     } else {
-      pos++; 
-      elem.style.top = pos + "px"; 
-      elem.style.left = pos + "px"; 
+      podPos += podMode * .45;
+      podPos += podMode * .45;
+      pod.style.top = podPos + "vmin"; 
+      pod.style.left = podPos + "vmin"; 
+      pod.style.width = podSize + "vmin";
+      pod.style.height = podSize + "vmin";
     }
   }
 }
