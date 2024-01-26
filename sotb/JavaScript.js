d322eb019968ae94d518f338eef07c49308a0bfb
Sabian Colbert <31480512+sabiancolbert@users.noreply.github.com>
2024-01-26 15:37:27 -0600

    Create JavaScript.js

---
+++ sotb/JavaScript.js
@@ -0,0 +1,16 @@
+function expandPodBox() {
+  let id = null;
+  const elem = document.getElementById("podButton");   
+  let pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 5);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.top = pos + "px"; 
+      elem.style.left = pos + "px"; 
+    }
+  }
+}
